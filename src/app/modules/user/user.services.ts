a7e00a1f1c599345414839a7e9d9cd1a46f08786
unknown <areyad203011@bscse.uiu.ac.bd>
2025-04-23 14:22:22 +0600

    register time token create done

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -2,6 +2,8 @@ import bcrypt from "bcrypt";
 import { IUser } from "./user.interface";
 import config from "../../config";
 import User from "./user.model";
+import { createToken } from "../auth/auth.utils";
+import { IJWTPayload } from "../auth/auth.interface";
 
 const registerUser = async (userInfo: IUser) => {
   const isUserExist = await User.findOne({ email: userInfo.email });
@@ -16,7 +18,22 @@ const registerUser = async (userInfo: IUser) => {
   userInfo.password = hashedPassword;
 
   const result = await User.create(userInfo);
-  return result;
+
+  const payload: IJWTPayload = {
+    email: userInfo.email,
+    userId: result?._id,
+  };
+
+  if (result) {
+    const accessToken = createToken(
+      payload,
+      config.jwt_secret!,
+      parseInt(config.jwt_expireTime!)
+    );
+    return accessToken;
+  }
+
+  throw new Error("Cannot register.");
 };
 
 export const userServices = {
